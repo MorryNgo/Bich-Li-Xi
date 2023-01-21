31e74dd33ba2134a14488d9cf85494bedca6bc5e
MorryNgo <103199602+MorryNgo@users.noreply.github.com>
2023-01-21 21:54:13 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const box = document.querySelector(".box")
+const thiep= document.querySelector("body > div > div.bia")
+const nap = document.querySelector("body > div > div.nap");
+const lixi = document.querySelector("body > div > div.li-xi")
+const song = document.getElementById("song")
+const heart = document.createElement("div")
+
+const text = ['2023','này',"hãy","để","anh","lì","xì","đời","trai","này","cho","em","nhé!",";3"]
+nap.addEventListener("click",open)
+
+function open(){
+    nap.style.animation='open linear 2s forwards';
+    setTimeout(function(){lixi.style.animation='push-up linear 2s forwards';   nap.style.zIndex=0;},3000)
+    setTimeout(function(){
+        thiep.style.animation='disappeared linear 1s forwards'
+        box.style.background= 'transparent'
+        nap.style.animation='disappeared linear 1s forwards'
+        setInterval(() => {
+         nap.remove()
+         thiep.remove()
+        }, 2000);
+    },5000)
+    setTimeout(function(){
+    
+        box.style.width="auto"
+   /*      lixi.innerHTML='bt' */
+        lixi.style.animation='sizeup linear 3s forwards'
+            setTimeout(() => {
+            for (let i = 0; i < text.length; i++){
+                setTimeout(() => {
+                    lixi.innerHTML += text[i]+" "
+                }, i*500);
+            }
+            }, 3000);
+            document.body.style.animation="changecolor linear 1s forwards"
+     /*        song.play() */
+            song.loop = true;
+        setInterval(function(){
+            heart.innerHTML="❤"
+            heart.classList.add("heart")
+            document.body.appendChild(heart)
+            heart.style.left =(Math.random()*150)+"vw"
+        },2000)
+},7000)
+}
+
